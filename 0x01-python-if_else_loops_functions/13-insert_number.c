235e64174225fc4dca9f419cd0d50a7ab4d55cd9
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-11-29 23:15:06 +0100

    Update 13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,97 +1,49 @@
 #include "lists.h"
 
-
-
 /**
- *
- *  * insert_node - inserts a new node
- *
- *   * at a given position.
- *
- *    * @head: head of a list.
- *
- *     * @number: index of the list where the new node is
- *
- *      * added.
- *
- *       * Return: the address of the new node, or NULL if it
- *
- *        * failed.
- *
- *         */
-
+ * insert_node - inserts a new node
+ * at a given position.
+ * @head: head of a list.
+ * @number: index of the list where the new node is
+ * added.
+ * Return: the address of the new node, or NULL if it
+ * failed.
+ */
 listint_t *insert_node(listint_t **head, int number)
-
 {
+	listint_t *new;
+	listint_t *h;
+	listint_t *h_prev;
 
-		listint_t *new;
+	h = *head;
+	new = malloc(sizeof(listint_t));
 
-			listint_t *h;
+	if (new == NULL)
+		return (NULL);
 
-				listint_t *h_prev;
+	while (h != NULL)
+	{
+		if (h->n > number)
+			break;
+		h_prev = h;
+		h = h->next;
+	}
 
+	new->n = number;
 
+	if (*head == NULL)
+	{
+		new->next = NULL;
+		*head = new;
+	}
+	else
+	{
+		new->next = h;
+		if (h == *head)
+			*head = new;
+		else
+			h_prev->next = new;
+	}
 
-					h = *head;
-
-						new = malloc(sizeof(listint_t));
-
-
-
-							if (new == NULL)
-
-										return (NULL);
-
-
-
-								while (h != NULL)
-
-										{
-
-													if (h->n > number)
-
-																	break;
-
-															h_prev = h;
-
-																	h = h->next;
-
-																		}
-
-
-
-									new->n = number;
-
-
-
-										if (*head == NULL)
-
-												{
-
-															new->next = NULL;
-
-																	*head = new;
-
-																		}
-
-											else
-
-													{
-
-																new->next = h;
-
-																		if (h == *head)
-
-																						*head = new;
-
-																				else
-
-																								h_prev->next = new;
-
-																					}
-
-
-
-												return (new);
-
+	return (new);
 }
