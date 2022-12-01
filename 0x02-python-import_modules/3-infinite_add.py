317b6d53efbe881f2d343d1feb843abed5e7285a
tobias@288 <tobiasuche7@gmail.com>
2022-12-01 07:50:23 -0800

    Completed Task

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if _name_ == "_main_":
+    import sys
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
---
+++ 0x02-python-import_modules/5-variable_load.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+if _name_ == "_main_":
+    from variable_load_5 import a
+    print(a)
