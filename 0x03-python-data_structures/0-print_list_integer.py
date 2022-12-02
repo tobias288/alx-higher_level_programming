3a46d963431a9412d27a39a30e6d9fa4d7995c9a
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-12-02 22:36:54 +0100

    Update 0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,4 @@
-st_integer(my_list=[]):
-
-        for i in range(len(my_list)):
-
-                    print("{:d}".format(my_list[i]))
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
