122dd7c92b76ddb8a5a4d49bbc1b6e807e45ec5e
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-12-03 06:14:13 +0100

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
-
 def multiple_returns(sentence):
+    len_sen = len(sentence)
 
-        len_sen = len(sentence)
+    if (len_sen == 0):
+        new_tuple = (len_sen, None)
+    else:
+        new_tuple = (len_sen, sentence[0])
 
-
-
-            if (len_sen == 0):
-
-                        new_tuple = (len_sen, None)
-
-                            else:
-
-                                        new_tuple = (len_sen, sentence[0])
-
-
-
-                                            return (new_tuple)
+    return (new_tuple)
