a7e3a7d8862df00233223ce60b43e0e8549904f1
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2022-12-03 05:58:00 +0100

    Update 5-number_keys.py

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
-
 def number_keys(a_dictionary):
+    num = 0
+    list_keys = list(a_dictionary.keys())
 
-        num = 0
+    for i in list_keys:
+        num += 1
 
-            list_keys = list(a_dictionary.keys())
-
-
-
-                for i in list_keys:
-
-                            num += 1
-
-
-
-                                return (num)
+    return (num)
