6268d0296adfb3f92e8ff6b93a871f15d4b66c40
tobias@288 <tobiasuche7@gmail.com>
2022-12-02 21:31:19 -0800

    Task Completed

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+
+        try:
+
+                    print("{:d}".format(value))
+
+                        except:
+
+                                    return (False)
+
+                                    else:
+
+                                                return (True)
