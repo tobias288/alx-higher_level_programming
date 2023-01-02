e36584f1abe75262c4108a4bdb269b7b3ff10953
Tobias Uchechukwu <100030817+tobias288@users.noreply.github.com>
2023-01-03 00:19:42 +0100

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,34 @@
 #!/usr/bin/python3
-class Sqaure:
-    """ A class that defines a square by its size
-    """
-    def __init__(self, size=0):
-        """Method to iinitialize the sqaure object
-        """
-        if not is isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be an in integer")
-        else:
-            self.__size = int(size)
+Square = __import__('2-square').Square
+
+my_square_1 = Square(3)
+print(type(my_square_1))
+print(my_square_1.__dict__)
+
+my_square_2 = Square()
+print(type(my_square_2))
+print(my_square_2.__dict__)
+
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_3 = Square("3")
+    print(type(my_square_3))
+    print(my_square_3.__dict__)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_4 = Square(-89)
+    print(type(my_square_4))
+    print(my_square_4.__dict__)
+except Exception as e:
+    print(e)
